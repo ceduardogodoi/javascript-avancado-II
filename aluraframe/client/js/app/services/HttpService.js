63d782327b5628ea9d548b0c16261e565c22910a
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-07-08 01:00:57 -0300

    Projeto finalizado

--- aluraframe/client/js/app/services/HttpService.js
+++ aluraframe/client/js/app/services/HttpService.js
@@ -1,22 +1,57 @@
 class HttpService {
+    
+    get(url) {
+        
+        return new Promise((resolve, reject) => {
+            
 
-  get(url) {
-    return new Promise((resolve, reject) => {
-      let xhr = new XMLHttpRequest();
-      xhr.open("GET", url);
+            let xhr = new XMLHttpRequest();
+            
+            xhr.open('GET', url);
+            
+            xhr.onreadystatechange = () => {
+                    
+                if(xhr.readyState == 4) {
+                    
+                    if(xhr.status == 200) {   
+                        
+                        resolve(JSON.parse(xhr.responseText));  
+                    } else {
+                        
+                        reject(xhr.responseText);
+                    }
+                }
+            };
+            
+            xhr.send();
+             
+            
+        });
+    }
+    
+    post(url, dado) {
 
-      // Configurações
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4) {
-          if (xhr.status == 200) {
-            resolve(JSON.parse(xhr.responseText));
-          } else {
-            reject(xhr.responseText);
-          }
+
+            return new Promise((resolve, reject) => {
+
+                let xhr = new XMLHttpRequest();
+                xhr.open("POST", url, true);
+                xhr.setRequestHeader("Content-type", "application/json");
+                xhr.onreadystatechange = () => {
+
+                    if (xhr.readyState == 4) {
+
+                        if (xhr.status == 200) {
+
+                            resolve(JSON.parse(xhr.responseText));
+                        } else {
+
+                            reject(xhr.responseText);
+                        }
+                    }
+                };
+                xhr.send(JSON.stringify(dado)); // usando JSON.stringifly para converter objeto em uma string no formato JSON.
+            });
+
         }
-      };
-      xhr.send();
-    });
-  }
-
 }
